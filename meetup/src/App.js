bbffe7800eadec79c89e126f3bbe39168a8b0325
TooniAdeagbo <80413283+TooniAdeagbo@users.noreply.github.com>
2021-12-15 19:09:54 +0000

    Update App.js

--- meetup/src/App.js
+++ meetup/src/App.js
@@ -1,120 +1,33 @@
-import './App.css';
-import React, {useState, useEffect} from "react";
-import {Link} from 'react-router';
-import firebase from './firebase.js';
-import 'firebase/firestore';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Profile from "./Pages/Profile";
-import Login from "./Pages/Login";
-import CreateEvent from "./Pages/CreateEvent";
+import "./styles.css";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Start from "./Pages/StartUp";
+import Register from "./Pages/SignUp";
+import Create from "./Pages/CreateEvent";
+import Update from "./Pages/UpdateEvent";
+import Home from "./Pages/Profile"; //Change to Profile
+import Login from "./Pages/SignIn"; //change to login
+import Search from "./Pages/searchResult";
+import WhoJoined from "./Pages/WhoJoined";
 import JoinEvent from "./Pages/JoinEvent";
+import Settings from "./Pages/ProfileSettings";
 
-class usersDisplay extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {userList : []}
-    }
-    render() {
-        return(
-         <div>
-
-         </div>
-        )
-    }
-}
 function App() {
-    //useState ==> ["Name of the set", Name of the function to set the set"]
-    const [users, setUser] = useState([]);
-    const [activities, setActivity] = useState([]);
-    const [events, setEvent] =  useState([]);
-    const [loading, setLoading] = useState(false);
-
-    //Database for users
-    const dbUsers = firebase.firestore().collection("user");
-
-    //Database for users
-    const dbActivity = firebase.firestore().collection("activity_type");
-
-    //Database for events 
-    const dbEvents = firebase.firestore().collection("event");
-
-    //Data retrieval function --> Loading phase
-    function getData() {
-        //Need to put on true
-        setLoading(true);
-        dbUsers.get().then((item)=> {
-            //Collecting data from the retrieval
-            const usersItems = item.docs.map((doc) => doc.data());
-            
-            //Setting the User List
-            setUser(usersItems);     
-        });
-
-
-        dbActivity.get().then((item)=> {
-            //Collecting data from the retrieval
-            const actItems = item.docs.map((doc) => doc.data());
-
-            //Setting the activity List
-            setActivity(actItems);
-            setLoading(false);
-        })
-    };
-
-    useEffect(() => {
-        getData();
-    },[]);
-
-    //Loading Screen
-    if (loading){
-        return <h1>Loading in progress...</h1>
-    }
-
-    return ( 
-        
-        <div>
-            <div>
-                <Router>
-                    <Switch>
-                        <Route exact path="/profile" component={Profile} />
-                        <Route exact path="/login" component={Login} />
-                        <Route exact path="/create" component={CreateEvent} />
-                        <Route exact path="/join" component={JoinEvent} />
-                    </Switch>
-                </Router>
-            </div>
-            <div>
-                <h2>List of users :</h2>
-                {users.map((u)=> (
-                    <div key={u.id}>
-                        - Username : {u.name}
-                    </div>
-                ))}
-            </div>
-            <div>
-                <h2>List of events :</h2>
-                {events.map((u)=> (
-                    <div key={u.Name}>
-                        - Name : {u.Name}
-                    </div>
-                ))}
-            </div>
-            <div>
-                <h2>List of activities :</h2>
-                {activities.map((u)=> (
-                    <div key={u.idActivity}>
-                        - Activities : {u.name} with id of {u.idActivity}
-                    </div>
-                ))}
-            </div>
-
-            {/* HTML code of the usersDisplay class */}
-            <usersDisplay/>
-        </div>
-        
-        
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/update" element={<Update />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/whojoined" element={<WhoJoined />} />
+        <Route path="/joinevent" element={<JoinEvent />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-export default  App;
-
-
+export default App;
